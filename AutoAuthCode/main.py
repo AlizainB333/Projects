4c9cefd237a686c05f467c76cf1f1d6f8d1b0fee
AZLilFire333 <98363538+AZLilFire333@users.noreply.github.com>
2025-08-05 22:54:36 -0700

    Main file is now hooked up to server and takes an email and password and logs into mailbox. Fetches emails based on limit. Then it will pass rawEmails into parser until code is found. Will be the entry point for the server when the OTP button is pressed or triggered to alert the server to parse and find the code for a certain email.

--- AutoAuthCode/main.py
+++ AutoAuthCode/main.py
@@ -1,17 +1,31 @@
 from email_fetch.client_login import connect_to_mailbox, fetch_unseen_mail, logout
-from email_parser import display_email_data, display_email_data_all_formats
+from email_parser import display_email_data, display_email_data_all_formats, decode_email
+
 
 EMAIL = "azainbandukwala333@gmail.com"
 APP_PASSWORD = "yrmf jife tqns bvwk".replace(" ", "")
 
-# Connect to the mailbox and fetch the data from emails
-mailBox = connect_to_mailbox(EMAIL, APP_PASSWORD)
-emails = fetch_unseen_mail(mailBox,1)
 
-#Display email data
-# display_email_data(emails)
-display_email_data_all_formats(emails)
+def get_otp_from_email(url=None):
+    # Connect to the mailbox and fetch the data from emails
+    mailBox = connect_to_mailbox(EMAIL, APP_PASSWORD)
+    rawEmails = fetch_unseen_mail(mailBox,10)
+
+    for rawEmail in rawEmails:
+        otpCode, otpFound = decode_email(rawEmail)
+        if otpFound:
+            return otpCode, otpFound
+        else:
+            continue
+    
+    return "OTP Not Found", False
 
 
+# authCode, foundFlag = get_otp_from_email()
+
+# print(authCode)
+    
+
+    
 
 
